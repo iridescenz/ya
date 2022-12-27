37002e00bf9c0c557c80ffe2936df26853336e18
Irina Turenko <turenko7irina@mail.ru>
2022-12-27 17:10:16 +0300

    netify added

---
+++ dist/index.9e12d7e1.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("#root");console.log(t),t&&(t.textContent=function(...t){if(0===t.length)throw Error("sum required at least 1 argument");return t.reduce(((t,e)=>t+e),0)}(6,-1).toString());
+//# sourceMappingURL=index.9e12d7e1.js.map
